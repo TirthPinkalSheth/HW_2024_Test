5dea8ccca2d65677ea1063c78d005ee9ed75e039
TirthPinkalSheth <tirthsheth100304@gmail.com>
2024-08-26 16:23:24 +0530

    main menu updates

--- Assets/Scripts/DoofusMovement.cs
+++ Assets/Scripts/DoofusMovement.cs
@@ -1,12 +1,13 @@
 using System.Diagnostics;
 using System.Security.Cryptography;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DoofusMovement : MonoBehaviour
 {
     private float speed;
     private Rigidbody rb;
-
+    public float fallLimit=-10f;
+    public Transform objectToCheck;
     void Start()
     {
         speed = ConfigManager.doofusDiary.player_data.speed;
@@ -15,11 +16,21 @@ public class DoofusMovement : MonoBehaviour
 
     void Update()
     {
+
         UnityEngine.Debug.Log(speed);
         float moveX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float moveZ = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
         Vector3 movement = new Vector3(moveX, 0, moveZ);
         transform.Translate(movement);
+        if (objectToCheck.position.y < fallLimit)
+        {
+            LoadNextScene();  // Call the function to load the next scene
+        }
+    }
+    void LoadNextScene()
+    {
+        // Load the game over scene
+        SceneManager.LoadScene("MainMenu");
     }
 }
